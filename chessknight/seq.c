387d065e1d1ce802eb82b335697eadf7867daffd
gregory <aanonimov117@gmail.com>
2022-11-16 01:31:30 +0300

    fix backtracing in seq algorithm

--- chessknight/seq.c
+++ chessknight/seq.c
@@ -1,97 +1,101 @@
 #include <mpi.h>
 #include "lib.h"
 
-int make_step(int **board, int n, int m, int pos_i, int pos_j, int done)
-{
-    int k = sizeof(steps) / sizeof(steps[0]);
-    int moves[k], index[k], count = 0;
-    int buf_i, buf_j, next_i, next_j;
-    for (int i = 0; i < k; i++)
-    {
-        moves[i] = 0;
-        index[i] = 0;
-        buf_i = pos_i + steps[i][0];
-        buf_j = pos_j + steps[i][1];
-        if (buf_i < 0 || buf_i >= n || buf_j < 0 || buf_j >= m || board[buf_i][buf_j] > 0)
-            continue;
-
-        int buf = 0;
-        for (int j = 0; j < k; j++)
-        {
-            next_i = buf_i + steps[j][0];
-            next_j = buf_j + steps[j][1];
-            if (next_i >= 0 && next_i < n && next_j >= 0 && next_j < m && board[next_i][next_j] == 0)
-                buf++;
-        }
-        next_j = 0;
-        for (int j = 0; j < count; j++)
-            if (buf >= moves[j])
-                next_j++;
-        for (int j = count; j > next_j; j--)
-        {
-            moves[j] = moves[j - 1];
-            index[j] = index[j - 1];
-        }
-        moves[next_j] = buf;
-        index[next_j] = i;
-        count++;
-    }
-    if (count == 0 || index[count - 1] == done)
-        return NO_SOLVE;
-    if (done < 0)
-        return index[0];
-    for (int i = 0; i < count; i++)
-        if (index[i] == done)
-            return index[i + 1];
-}
+int steps[][2] = {{-1, -2}, {-2, -1}, {-2, 1}, {1, -2}, {-1, 2}, {2, -1}, {1, 2}, {2, 1}};
 
 // int make_step(int **board, int n, int m, int pos_i, int pos_j, int done)
 // {
-//     int last = sizeof(steps) / sizeof(steps[0]);
-//     if (++done == last)
-//         return NO_SOLVE;
-
-//     for (int i = done; i < last; i++)
+//     int k = sizeof(steps) / sizeof(steps[0]);
+//     int moves[k], index[k], count = 0;
+//     int buf_i, buf_j, next_i, next_j;
+//     for (int i = 0; i < k; i++)
 //     {
-//         int buf_i = pos_i + steps[i][0];
-//         int buf_j = pos_j + steps[i][1];
+//         moves[i] = 0;
+//         index[i] = 0;
+//         buf_i = pos_i + steps[i][0];
+//         buf_j = pos_j + steps[i][1];
 //         if (buf_i < 0 || buf_i >= n || buf_j < 0 || buf_j >= m || board[buf_i][buf_j] > 0)
 //             continue;
-//         return i;
+
+//         int buf = 0;
+//         for (int j = 0; j < k; j++)
+//         {
+//             next_i = buf_i + steps[j][0];
+//             next_j = buf_j + steps[j][1];
+//             if (next_i >= 0 && next_i < n && next_j >= 0 && next_j < m && board[next_i][next_j] == 0)
+//                 buf++;
+//         }
+//         next_j = 0;
+//         for (int j = 0; j < count; j++)
+//             if (buf >= moves[j])
+//                 next_j++;
+//         for (int j = count; j > next_j; j--)
+//         {
+//             moves[j] = moves[j - 1];
+//             index[j] = index[j - 1];
+//         }
+//         moves[next_j] = buf;
+//         index[next_j] = i;
+//         count++;
 //     }
-//     return NO_SOLVE;
+//     if (count == 0 || index[count - 1] == done)
+//         return NO_SOLVE;
+//     if (done < 0)
+//         return index[0];
+//     for (int i = 0; i < count; i++)
+//         if (index[i] == done)
+//             return index[i + 1];
 // }
+
+int make_step(int **board, int n, int m, int pos_i, int pos_j, int done)
+{
+    int last = sizeof(steps) / sizeof(steps[0]);
+    if (++done == last)
+        return NO_SOLVE;
+
+    for (int i = done; i < last; i++)
+    {
+        int buf_i = pos_i + steps[i][0];
+        int buf_j = pos_j + steps[i][1];
+        if (buf_i < 0 || buf_i >= n || buf_j < 0 || buf_j >= m || board[buf_i][buf_j] > 0)
+            continue;
+        return i;
+    }
+    return NO_SOLVE;
+}
 
 int find_path(int **board, int n, int m, int pos_i, int pos_j)
 {
-    int find = FALSE, max_step = n * m, step = 1, step_index = 0;
-    int **step_done = alloc_matrix(n, m);
-    board[pos_i][pos_j] = 1;
-    step_done[pos_i][pos_j] = -1;
+    int find = FALSE, max_step = n * m, step = 0, step_index = 0;
+    int *step_done = malloc(max_step-- * sizeof(int));
+    step_done[step] = -1;
     while (step < max_step)
     {
-        step_index = make_step(board, n, m, pos_i, pos_j, step_done[pos_i][pos_j]);
+        step_index = make_step(board, n, m, pos_i, pos_j, step_done[step]);
         if (step_index >= 0)
         {
-            step_done[pos_i][pos_j] = step_index;
+            step_done[step] = step_index;
+            board[pos_i][pos_j] = ++step;
             pos_i += steps[step_index][0];
             pos_j += steps[step_index][1];
-            board[pos_i][pos_j] = ++step;
-            step_done[pos_i][pos_j] = -1;
+            step_done[step] = -1;
         }
         else if (step > 0)
         {
             board[pos_i][pos_j] = 0;
-            pos_i -= steps[step_done[pos_i][pos_j]][0];
-            pos_j -= steps[step_done[pos_i][pos_j]][1];
             step--;
+            pos_i -= steps[step_done[step]][0];
+            pos_j -= steps[step_done[step]][1];
         }
         else
             break;
     }
     if (step == max_step)
+    {
+        board[pos_i][pos_j] = ++step;
         find = TRUE;
-    free_matrix(step_done, n);
+    }
+    free(step_done);
     return find;
 }
 
@@ -125,8 +129,8 @@ int solve(int **board, int n, int m)
 
 int main(int argc, char **argv)
 {
-    int sizes[][2] = {{3, 4}, {4, 5}, {5, 5}, {3, 7}, {7, 7}, {8, 8}, {10, 10}, {3, 14}, {15, 15}, {25, 25}, 
-                    {39, 39}, {50, 50}, {64, 64}, {73, 73}, {100, 100}, {200, 200}, {500, 500}, {1000, 1000}};
+    int sizes[][2] = {{3, 4}, {4, 5}, {5, 5}, {3, 7}, {7, 7}, {8, 8}, /*{10, 10}, {3, 14}, {15, 15}, {25, 25}, 
+                    {39, 39}, {50, 50}, {64, 64}, {73, 73}, {100, 100}, {200, 200}, {500, 500}, {1000, 1000}*/};
     int n = sizeof(sizes) / sizeof(sizes[0]);
     FILE *file = stdout;
     if (!file)
