67546b4dd1e58cb12f58e80beb9ef09ee4c7dc69
Grigoriy Budkin <g.budkin@vk.team>
2022-11-28 17:07:23 +0300

    beginning of parallel solution

--- chessknight/par.c
+++ chessknight/par.c
@@ -1,18 +1,44 @@
 #include <mpi.h>
 #include "lib.h"
 
-int steps[][2] = {{-1, -2}, {-2, -1}, {-2, 1}, {1, -2}, {-1, 2}, {2, -1}, {1, 2}, {2, 1}};
-
 /*
 2 types of process - main and workers.
-2 queues - tasks and results.
 in the beginning main generates tasks by worker count and put them in task queue.
 workers get task from queue and do some job. result send in results queue.
 main check result state: if solved - finish, else generate new task(s).
 */
 
-int solve(int **board, int n, int m)
+int solve(int **board, int n, int m, int rank, int nprocs)
 {
+    if (check_sizes(n, m) != OK)
+        return NO_SOLVE;
+
+    if (rank == 0)
+    {
+        int n2 = (n + 1) / 2, m2 = (m + 1) / 2, find = FALSE;
+        int i = 0, j = -1;
+        while (!find)
+        {
+            j++;
+            if (j == m2)
+            {
+                j = 0;
+                i++;
+            }
+            if (i == n2)
+                break;
+            if (n * m % 2 && (i + j) % 2)
+                continue;
+
+            /*find = find_solution(board, n, m, i, j, rank, nprocs)) //заменить на параллельную обработку
+            if (!find)
+                zero_matrix(board, n, m);*/ 
+        }
+    }
+    else 
+    {
+
+    }
     return OK;
 }
 
@@ -25,15 +51,19 @@ int main(int argc, char **argv)
     if (!file)
         return NO_SOLVE;
     
+    int myrank, nprocs;
     MPI_Init(&argc, &argv);
-    /*for (int i = 0; i < n; i++)
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    for (int i = 0; i < n; i++)
     {
         int **matrix = alloc_matrix(sizes[i][0], sizes[i][1]);
         double time = 0, t = 0;
         for (int j = 0; j < N; j++)
         {
             t = MPI_Wtime();
-            solve(matrix, sizes[i][0], sizes[i][1]);
+            solve(matrix, sizes[i][0], sizes[i][1], myrank, nprocs);
             time += MPI_Wtime() - t;
             if (j < N - 1)
                 zero_matrix(matrix, sizes[i][0], sizes[i][1]);
@@ -42,14 +72,8 @@ int main(int argc, char **argv)
         print_matrix(file, matrix, sizes[i][0], sizes[i][1]);
         fprintf(file, "Time for solution: %lf\n", time / N);
         free_matrix(matrix, sizes[i][0]);
-    }*/
-    int myrank, nprocs, len;
-	char name[MPI_MAX_PROCESSOR_NAME];
+    }
 
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	MPI_Get_processor_name(name, &len);
-    printf("Hello from processor %s[%d] %d of %d  \n", name, len, myrank, nprocs);
     MPI_Finalize();
     fclose(file);
     return OK;
